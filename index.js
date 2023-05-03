a5c2e842279faa8eb2661fa603254f7acc80081c
rabiul-ahsan <ra.rabiulahsan@gmail.com>
2023-05-03 16:04:31 +0600

    server started

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+// const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Chefs House API is running on port: ${port}`);
+});
